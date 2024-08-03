03603604b0f0e4a484c3ca4cf89d3a7f1f712173
susilreact <jenasusil@outlook.com>
2024-08-03 21:19:55 +0530

    successfully fetch data from srver using product.js file

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,22 @@
-import React from 'react'
+import React from 'react';
+import { Container } from 'react-bootstrap';
+import Header from './components/Header';
+import Footer from './components/Footer';
+// import HomeScreen from './screen/HomeScreen';
+import { Outlet } from 'react-router-dom';
 
 const App = () => {
-  return (
-    <h1>Welcome to ....</h1>
-  )
-}
+    return (
+        <div>
+            <Header />
+            <main className='py-3'>
+                <Container>
+                    <Outlet/>
+                </Container>
+            </main>
+            <Footer />
+        </div>
+    );
+};
 
-export default App
+export default App;
--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,13 +1,28 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
+//import 'bootstrap/dist/css/bootstrap.min.css';
+import {createBrowserRouter,createRoutesFromElements,Route,RouterProvider} from 'react-router-dom'
+import './assets/styles/bootstrap.custom.css';
+import './assets/styles/index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import HomeScreen from './screen/HomeScreen';
+import ProductScreen from './screen/ProductScreen';
+
+
+const router = createBrowserRouter(
+    createRoutesFromElements(
+        <Route path='/' element={<App />}>
+            <Route index={true} path='/' element={<HomeScreen />} />
+            <Route  path='/product/:id' element={<ProductScreen />} />
+        </Route>
+    )
+);
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>
 );
 
