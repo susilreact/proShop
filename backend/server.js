5a3d8cf7b4d5e1a5c8e14cbb8a45458b7baa7078
susilreact <jenasusil@outlook.com>
2024-08-04 14:05:40 +0530

    backend model router and controller set up

--- backend/server.js
+++ backend/server.js
@@ -1,8 +1,12 @@
 import express from 'express';
-import products from './data/products.js'
+import { notFound,errorHandler } from './middleware/errorMiddleware.js';
+import connectDB from './config/db.js';
 import dotenv from 'dotenv';
+import productRoutes from './routes/productRoutes.js'
 dotenv.config();
 const port = process.env.PORT || 5000;
+
+connectDB(); //Connect to MongoDB
 
 const app = express();
 
@@ -10,12 +14,10 @@ app.get('/',(req,res)=>{
     res.send('API is running')
 })
 
-app.get('/api/products',(req,res)=>{
-    res.json(products)
-})
-app.get('/api/products/:id', (req, res) => {
-    const product = products.find((product)=> product._id === req.params.id);
-    res.json(product);
-});
+app.use('/api/products',productRoutes);
+
+app.use(notFound)
+app.use(errorHandler)
+
 
 app.listen(port,()=>console.log(`server running on port ${port}`))
